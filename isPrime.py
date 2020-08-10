9a426bad318021f28e50a927d2c9eff4383483d3
code-simple <61619216+code-simple@users.noreply.github.com>
2020-08-11 00:00:50 +0500

    Update isPrime.py

--- isPrime.py
+++ isPrime.py
@@ -1,14 +1,14 @@
 def isPrime(n):
-    if n==1:
-        return False
-    elif n==2:
+    if n==2 or n==3 or n==5:
         return True
+    elif n==1 or n%2==0 or n%3==0 or n%5==0:
+        return False
     else:
         for x in range(2,n):
             if n%x==0:
                 return False
         return True
 
-for i in range(1, 100):
-	if isPrime(i + 1):
-			print(i + 1, end=" ")
+for i in range(1, 6850):
+	if isPrime(i):
+		print(i, end=" ")
